da12198d6b42993f682f1fecc91896a555aeb372
Adwait1825 <adwaitkhaladkar18@gmail.com>
2025-07-27 18:31:46 +0200

    Update rover_server.py

--- rover_server.py
+++ rover_server.py
@@ -36,16 +36,19 @@ def generate_frames():
     while True:
         success, frame = cap.read()
         if not success:
+            print("Failed to grab frame")
             break
         _, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
-        yield (b'--frame\\r\\n'
-               b'Content-Type: image/jpeg\\r\\n\\r\\n' + frame + b'\\r\\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route("/video")
 def video():
     return Response(generate_frames(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
+
 
 @app.route("/start", methods=["POST"])
 def start_rover():
@@ -59,4 +62,3 @@ def stop_rover():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
